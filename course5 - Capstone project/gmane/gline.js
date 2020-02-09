886372af75963e10672e43a6f8af9d1c86679055
Mouhamadou GUEYE <mogueye87@gmail.com>
2020-02-09 13:58:14 +0100

    capstone project

---
+++ course5 - Capstone project/gmane/gline.js
@@ -0,0 +1,5 @@
+gline = [ ['Month','umich.edu','indiana.edu','ucdavis.edu','berkeley.edu','uct.ac.za','ufp.pt','columbia.edu','mac.com','gmail.com','virginia.edu'],
+['2005-12',57,12,11,12,14,10,13,12,10,9],
+['2006-01',114,32,37,33,27,32,28,14,23,21],
+['2006-02',30,10,4,7,7,3,3,16,7,7]
+];
